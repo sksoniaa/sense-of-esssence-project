3f13887f39827abf207940e0ff30d55d1975e23c
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-03-14 22:16:09 -0500

    navBar completed, responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,36 @@
 import { Link } from 'react-router-dom'
+import { useState, useRef } from 'react'
+import { IoMenu } from "react-icons/io5";
+import { FaTimes } from "react-icons/fa";
 import './Header.css'
 
 export default function Header() {
 
-  return(
-    <nav className="nav">
-      <a href="/" className='site-title'>Sense of Essence</a>
+  const responsiveNav = useRef()
 
-      <ul>
-        <li><Link to='/samplings'>Sampling</Link></li>
-        <li><Link to='/new'>New</Link></li>
-        <li><Link to='/trending'>Trending</Link></li>
-      </ul>
-    </nav>
+  function responsiveNavFunc() {
+    responsiveNav.current.classList.toggle("responsiveNav")
+  }
+
+  return(
+    <>
+    <div className="header">
+        <Link to="/" className='site-title'>Sense of Essence</Link>
+
+        <div className="ul" ref={responsiveNav}>
+          <Link to='/samplings'>Sampling</Link>
+          <Link to='/new'>New</Link>
+          <Link to='/trending'>Trending</Link>
+        <button className="close" onClick={responsiveNavFunc}>
+          <FaTimes />
+        </button>
+        </div>
+
+      <button className='open'  onClick={responsiveNavFunc}>
+        <IoMenu />
+      </button>
+
+    </div>
+    </>
   )
 }
